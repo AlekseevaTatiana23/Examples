560883a74245579e5e71c028f790d5bf839b5c26
Tatiana Alekseeva <alekseeva.tany@list.ru>
2023-02-27 22:41:11 +0300

    после семинара 2

---
+++ Task03/Program.cs
@@ -0,0 +1,39 @@
+﻿//3. Напишите программу, которая будет выдавать
+//название дня недели по заданному номеру.
+//3 -> Среда
+//5 -> Пятница
+
+Console.WriteLine("введите день недели");
+string day = Console.ReadLine();
+if (day = 1)
+{
+    Console.WriteLine("понедельник");
+}
+ else if (day = 2)
+{
+    Console.WriteLine("вторник");
+}
+else if (day = 3)
+{
+    Console.WriteLine("среда");
+}
+else if (day = 4)
+{
+    Console.WriteLine("четверг");
+}
+else if (day = 5)
+{
+    Console.WriteLine("пятница");
+}
+else if (day = 6)
+{
+    Console.WriteLine("суббота");
+}
+else if (day = 7)
+{
+    Console.WriteLine("воскресенье");
+}
+else
+{
+    Console.WriteLine("некорректный ввод");
+}
---
+++ Task13/Program.cs
@@ -0,0 +1,33 @@
+﻿// /12. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли первое число
+// кратным второму. Если число 1 не кратно числу 2, то
+// программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.WriteLine("Введите первое чило: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе чило: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int result = number % number1;
+if(result == 0)
+{
+    Console.WriteLine($"Число {number}  кратно числу {number1}");
+}
+else
+{
+    Console.WriteLine($"Остаток от деления {result}");
+}
+
+//второй вариант-функция
+int delSecondDigit = DelSecondDigit(number);
+Console.WriteLine($"Полученное двухзначное число -> {delSecondDigit}");
+
+int DelSecondDigit(int num)
+{
+    int firstDigit = num / 100;
+    int lastDigit = num % 10;
+    int sumD = firstDigit * 10 + lastDigit;
+    return sumD;
+}
